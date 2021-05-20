4f212c31790c39f360e354e3db47b24fc6deb1d6
W-A-James <wajames@princeton.edu>
2021-05-20 00:24:13 -0400

    Fixed breaking bug where init_frame was adding an additional line to an image

--- src/image_manip.rs
+++ src/image_manip.rs
@@ -20,14 +20,13 @@ fn push_pixel(vec: &mut Vec<u8>, colour: Colour) {
     });
 }
 
-fn init_image(
+pub fn init_image(
     width: u16,
     height: u16,
     mut init_line: &mut BitMap,
 ) -> Result<Vec<u8>, EncodingError> {
-    let num_pixels:usize = (width as usize) * (height as usize);
+    let num_pixels: usize = (width as usize) * (height as usize);
     let mut image: Vec<u8> = Vec::with_capacity(num_pixels);
-    *init_line = bitmap::rule110_step(&mut init_line);
     for _y in 0..height {
         for x in 0..width {
             if init_line.get(x.into()) == 1 {
@@ -38,39 +37,37 @@ fn init_image(
         }
         *init_line = bitmap::rule110_step(&mut init_line);
     }
-    debug_assert!(image.len() == num_pixels);
+    assert!(image.len() == num_pixels);
     // Return new frame
     Ok(image)
 }
 
-fn gen_next_image(
+pub fn gen_next_image(
     image: &mut Vec<u8>,
     width: u16,
+    height: u16,
     mut line: &mut BitMap,
 ) -> Result<(), EncodingError> {
-    let first_row_len:usize = width.into();
+    let first_row_len: usize = width.into();
     // delete first row
     image.drain(0..first_row_len);
 
     *line = bitmap::rule110_step(&mut line);
     for x in 0..width {
-        if line.get(x.into()) == 1 {
-            push_pixel(image, Colour::BLACK);
-        } else {
-            push_pixel(image, Colour::WHITE);
+        match line.get(x.into()) {
+            1 => push_pixel(image, Colour::BLACK),
+            0 => push_pixel(image, Colour::WHITE),
+            _ => panic!(),
         }
     }
+    debug_assert!(image.len() == (width as usize) * (height as usize));
 
     Ok(())
 }
 
 // TODO: check if this works as intended
 fn build_frame(width: u16, height: u16, img: &[u8]) -> Frame {
-    let mut frame = Frame::default();
-    frame.width = width;
-    frame.height = height;
-    frame.buffer = Cow::Borrowed(&*img);
-
+    let frame = Frame::from_indexed_pixels(width, height, img, None);
     frame
 }
 
@@ -97,7 +94,7 @@ pub fn build_gif(
         Some(progress_bar_tx) => {
             // iterate over other frames
             for s in 1..steps {
-                gen_next_image(&mut img, width, &mut init_line).unwrap();
+                gen_next_image(&mut img, width, height, &mut init_line).unwrap();
                 let frame = build_frame(width, height, &img);
                 encoder.write_frame(&frame).unwrap();
                 // Update progress bar
@@ -105,168 +102,15 @@ pub fn build_gif(
             }
             // Finish updating progress bar
             progress_bar_tx.send(steps - 1).unwrap();
-        },
+        }
         None => {
             for _ in 1..steps {
-                gen_next_image(&mut img, width, &mut init_line).unwrap();
+                gen_next_image(&mut img, width, height, &mut init_line).unwrap();
                 let frame = build_frame(width, height, &img);
                 encoder.write_frame(&frame).unwrap();
             }
         }
-    } 
-    
+    }
+
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn init() {
-        env_logger::init();
-    }
-
-    #[test]
-    fn test_init_image() {
-        let mut bmp_0 = BitMap::new(10);
-        let mut bmp_1 = BitMap::new(10);
-
-        for _ in 0..10 {
-            bmp_1 = bitmap::rule110_step(&mut bmp_1);
-        }
-        init_image(10, 11, &mut bmp_0).unwrap();
-
-        let vec_0 = bmp_0.get_vec();
-        let vec_1 = bmp_1.get_vec();
-
-        assert!(vec_0.len() == vec_1.len());
-        for i in 0..vec_0.len() {
-            assert!(vec_0[i] == vec_1[i]);
-        }
-    }
-
-    #[test]
-    fn test_gen_next_image() {
-        let mut bmp_0 = BitMap::new(10);
-        let mut bmp_1 = BitMap::new(10);
-        for _ in 0..11 {
-            bmp_1 = bitmap::rule110_step(&mut bmp_1);
-        }
-        let mut img = init_image(10, 11, &mut bmp_0).unwrap();
-        gen_next_image(&mut img, 10, &mut bmp_0).unwrap();
-
-        let vec_0 = bmp_0.get_vec();
-        let vec_1 = bmp_1.get_vec();
-
-        assert!(vec_0.len() == vec_1.len());
-        for i in 0..vec_0.len() {
-            assert!(vec_0[i] == vec_1[i]);
-        }
-    }
-
-    use log::{info, error};
-    use std::time::Instant;
-    // TODO: make this more useful
-    #[test]
-    fn test_build_gif() {
-        init();
-        // Ensure that all images end in 0x3b
-        let sizes = vec![
-            (10, 10),
-            (20, 20),
-            (40, 40),
-            (80, 80),
-            (160, 160),
-            (320, 320),
-            (640, 640),
-            (1280, 1280),
-            (2560, 2560),
-            (5120, 5120),
-            (10240, 10240),
-        ];
-
-        for size in sizes {
-            let w = size.0 as u16;
-            let h = size.1 as u16;
-            let steps = 10;
-            let file_name = format!("test_{}x{}.gif", w, h);
-            let mut bmp = BitMap::random(w.into());
-
-            let start = Instant::now();
-            build_gif(w, h, steps, &mut bmp, file_name.as_str(), None).unwrap();
-            let end = Instant::now();
-            info!(
-                "{}",
-                format!(
-                    "w: {}, h: {}, steps: {}, time_elapsed: {:?}\n",
-                    w,
-                    h,
-                    steps,
-                    end.duration_since(start)
-                )
-            );
-
-            info!("{}", format!("Attempting to read {}", file_name));
-            let file = match File::open(file_name.as_str()) {
-                Ok(f) => {
-                    info!("Successfully opened {}", file_name);
-                    f
-                },
-                Err(e) => {
-                    info!("Error: {:?}", e);
-                    panic!(e);
-                }
-            };
-            let mut gif_opts = gif::DecodeOptions::new();
-            gif_opts.set_color_output(gif::ColorOutput::Indexed);
-            let mut decoder = match gif_opts.read_info(file) {
-                Ok(d) => {
-                    info!("Successfully created decoder!");
-                    d
-                },
-                Err(e) => {
-                    error!("Error: {:?}", e);
-                    panic!(e);
-                }
-            };
-            let mut screen = gif_dispose::Screen::new_decoder(&decoder);
-
-            let mut i = 0;
-            let mut exit_loop = false;
-            while !exit_loop {
-                assert!(decoder.width() == w);
-                assert!(decoder.height() == h);
-                let palette = decoder.global_palette().unwrap();
-                // Make sure that global palette is valid
-                // [0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00]
-                for j in 0..3 {
-                    assert!(0xFF == palette[j]); 
-                    assert!(0x00 == palette[5 - j]);
-                }
-                let frame = decoder.read_next_frame();
-                match frame {
-                    Ok(img) => {
-                        info!("{}", format!("Successfully read frame {}\n", i));
-                        match img {
-                            Some(image) => {
-                                assert!(image.width == w);
-                                assert!(image.height == h);
-
-                                screen.blit_frame(&image).unwrap();
-                            }
-                            None => {
-                                exit_loop = true;
-                            }
-                        };
-                    }
-                    Err(e) => {
-                        error!("{}", format!("Failed to read frame {}: {:?}", i, e));
-                        panic!("{:?}", e);
-                    }
-                }
-                i += 1;
-            }
-            info!("{}", format!("Successfully read {}\n", file_name));
-        }
-    }
 }
--- src/image_manip/bitmap.rs
+++ src/image_manip/bitmap.rs
@@ -3,7 +3,7 @@ use rand::Rng;
 #[derive(Debug)]
 pub struct BitMap {
     vals: Vec<u64>, // Vector of u64s
-    len: u64,     // number of accessible bits
+    len: u64,       // number of accessible bits
 }
 
 // Arbitrary size bitmap where most significant bit is leftmost
@@ -74,7 +74,7 @@ impl BitMap {
     }
 
     pub fn set_vec_val(&mut self, index: u64, val: u64) {
-        if index >= self.vals.len() as u64{
+        if index >= self.vals.len() as u64 {
             panic!("Error! Vector overflow");
         }
         self.vals[index as usize] = val;
@@ -86,12 +86,13 @@ impl BitMap {
         }
 
         let index = bit_num / 64;
-        let offset= bit_num % 64;
+        let offset = bit_num % 64;
+        println!("bit_num: {}\nindex: {}\noffset: {}", bit_num, index, offset);
 
         self.vals[index as usize] &= !(1 << offset);
     }
 
-    pub fn size(&self) -> u64{
+    pub fn size(&self) -> u64 {
         self.len
     }
 
@@ -102,8 +103,7 @@ impl BitMap {
     pub fn to_bit_vec(&self) -> Vec<u8> {
         let mut bit_vec: Vec<u8> = Vec::with_capacity(self.len as usize);
         for i in 0..self.len {
-            let i_usize = i as usize;
-            bit_vec[i_usize] = self.get(i);
+            bit_vec.push(self.get(i));
         }
         bit_vec
     }
@@ -123,12 +123,8 @@ pub fn rule110_step(bmp: &mut BitMap) -> BitMap {
         let mut flags: u8 = 0;
         for offset in [Offset::PlusOne, Offset::Zero, Offset::MinusOne].iter() {
             let (flag_mask, index) = match offset {
-                Offset::PlusOne  => {
-                    (0b100, (i + 1) % bmp.size())
-                },
-                Offset::Zero => {
-                    (0b010, i)
-                },
+                Offset::PlusOne => (0b100, (i + 1) % bmp.size()),
+                Offset::Zero => (0b010, i),
                 Offset::MinusOne => {
                     if i == 0 {
                         (0b001, len - 1)
@@ -146,286 +142,7 @@ pub fn rule110_step(bmp: &mut BitMap) -> BitMap {
 
         if new_val != 0 {
             rv.set(i);
-        } 
+        }
     }
     rv
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_constructor() {
-        let mut bmp = BitMap::new(55);
-        assert!(bmp.size() == 55);
-        let vec = bmp.get_vec();
-        assert!(vec.len() == 1);
-
-        bmp = BitMap::new(1500);
-        assert!(bmp.size() == 1500);
-        let vec = bmp.get_vec();
-        assert!(vec.len() == 24);
-
-        bmp = BitMap::new(64);
-        assert!(bmp.size() == 64);
-        let vec = bmp.get_vec();
-        assert!(vec.len() == 1);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_constructor_bad_args() {
-        BitMap::new(0);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_random_constructor_bad_args() {
-        BitMap::random(0);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_get_with_out_of_bounds_val() {
-        let bmp = BitMap::random(10);
-        bmp.get(10);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_set_with_out_of_bounds_val() {
-        let mut bmp = BitMap::random(10);
-        bmp.set(10);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_unset_with_out_of_bounds_val() {
-        let mut bmp = BitMap::random(10);
-        bmp.unset(10);
-    }
-
-    #[test]
-    fn test_set() {
-        let mut bmp = BitMap::new(64);
-        let mut cur_val: u64 = 0;
-        for i in 0..64 {
-            bmp.set(i);
-            let vec = bmp.get_vec();
-            cur_val += 2u64.pow(i as u32);
-            assert!(vec[0] == cur_val);
-        }
-    }
-
-    #[test]
-    fn test_unset() {
-        let mut bmp = BitMap::new(64);
-        let mut cur_val: u64 = u64::MAX;
-        for i in 0..64 {
-            bmp.set(i);
-        }
-
-        for i in 0..64 {
-            bmp.unset(i);
-            cur_val -= 2u64.pow(i as u32);
-            let vec = bmp.get_vec();
-            assert!(vec[0] == cur_val);
-        }
-    }
-
-    #[test]
-    fn test_rule110_step_normal_cases() {
-        let mut bmp = BitMap::new(3);
-        // 000
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(1) == 0);
-        // 001
-        let mut bmp = BitMap::new(3);
-        bmp.set(0);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(1) == 1);
-        // 010
-        let mut bmp = BitMap::new(3);
-        bmp.set(1);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(1) == 1);
-        // 011
-        let mut bmp = BitMap::new(3);
-        bmp.set(1);
-        bmp.set(0);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(1) == 1);
-        // 100
-        let mut bmp = BitMap::new(3);
-        bmp.set(2);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(1) == 0);
-        // 101
-        let mut bmp = BitMap::new(3);
-        bmp.set(2);
-        bmp.set(0);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(1) == 1);
-        // 110
-        let mut bmp = BitMap::new(3);
-        bmp.set(2);
-        bmp.set(1);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(1) == 1);
-        // 111
-        let mut bmp = BitMap::new(3);
-        bmp.set(2);
-        bmp.set(1);
-        bmp.set(0);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(1) == 0);
-    }
-
-    #[test]
-    fn test_rule110_step_wrap_around_cases() {
-        // Testing bit 2
-        let mut bmp = BitMap::new(3);
-        // 000
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(2) == 0);
-        // 001
-        let mut bmp = BitMap::new(3);
-        bmp.set(1);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(2) == 1);
-        // 010
-        let mut bmp = BitMap::new(3);
-        bmp.set(2);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(2) == 1);
-        // 011
-        let mut bmp = BitMap::new(3);
-        bmp.set(1);
-        bmp.set(2);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(2) == 1);
-        // 100
-        let mut bmp = BitMap::new(3);
-        bmp.set(0);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(2) == 0);
-        // 101
-        let mut bmp = BitMap::new(3);
-        bmp.set(1);
-        bmp.set(0);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(2) == 1);
-        // 110
-        let mut bmp = BitMap::new(3);
-        bmp.set(2);
-        bmp.set(0);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(2) == 1);
-        // 111
-        let mut bmp = BitMap::new(3);
-        bmp.set(2);
-        bmp.set(1);
-        bmp.set(0);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(1) == 0);
-
-        // Testing bit 0
-        let mut bmp = BitMap::new(3);
-        // 000
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(0) == 0);
-        // 001
-        let mut bmp = BitMap::new(3);
-        bmp.set(2);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(0) == 1);
-        // 010
-        let mut bmp = BitMap::new(3);
-        bmp.set(0);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(0) == 1);
-        // 011
-        let mut bmp = BitMap::new(3);
-        bmp.set(0);
-        bmp.set(2);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(0) == 1);
-        // 100
-        let mut bmp = BitMap::new(3);
-        bmp.set(1);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(0) == 0);
-        // 101
-        let mut bmp = BitMap::new(3);
-        bmp.set(1);
-        bmp.set(2);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(0) == 1);
-        // 110
-        let mut bmp = BitMap::new(3);
-        bmp.set(1);
-        bmp.set(0);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(0) == 1);
-        // 111
-        let mut bmp = BitMap::new(3);
-        bmp.set(2);
-        bmp.set(1);
-        bmp.set(0);
-        bmp = rule110_step(&mut bmp);
-        assert!(bmp.get(0) == 0);
-    }
-
-    use std::time::Duration;
-    use std::time::Instant;
-    #[test]
-    fn rule_110_profiling() {
-        let sizes = vec![64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768];
-        let len = sizes.len();
-        let mut rule110_times: Vec<Duration> = Vec::with_capacity(sizes.len());
-        let num_iterations = 1000;
-
-        for size in &sizes {
-            let mut bmp = BitMap::random(*size);
-            let start = Instant::now();
-            for _ in 0..num_iterations {
-                bmp = rule110_step(&mut bmp);
-            }
-            let end = Instant::now();
-            rule110_times.push(end.duration_since(start));
-        }
-
-        for i in 0..len {
-            println!(
-                "N: {}, rule110_step: {}s",
-                sizes[i], rule110_times[i].as_secs_f64()/(num_iterations as f64)
-            );
-        }
-    }
-
-    #[test]
-    fn constructor_profiling() {
-        let sizes: Vec<u64> = vec![64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768];
-        let len = sizes.len();
-        let mut constructor_times: Vec<Duration> = Vec::with_capacity(sizes.len());
-        let num_iterations = 1000;
-
-        for size in &sizes {
-            let start = Instant::now();
-            for _ in 0..num_iterations {
-                let _bmp = BitMap::random(*size);
-            }
-            let end = Instant::now();
-
-            constructor_times.push(end.duration_since(start));
-        }
-
-        for i in 0..len {
-            println!(
-                "N: {}, constructor: {:?}",
-                sizes[i], constructor_times[i].as_secs_f64()/(num_iterations as f64)
-            );
-        }
-    }
 }
